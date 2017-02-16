0137c1dbd390d9beabc97c1849b49731ce10f7ff
bclouse <bclouse@nevada.unr.edu>
2017-02-16 11:14:14 -0800

    Initial upload

---
+++ Classes.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <assert.h>
+#include <random>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+#ifndef CLASSES_H
+#define CLASSES_H
+
+#endif
---
+++ hw2.cpp
@@ -0,0 +1,21 @@
+//
+//	Bryant Clouse
+//	Multi-armed bandit
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <assert.h>
+#include <random>
+#include <iomanip>
+#include <cmath>
+#include "Classes.h"
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}
